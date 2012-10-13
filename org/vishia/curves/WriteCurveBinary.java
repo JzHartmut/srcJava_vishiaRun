768f7952692fc5e38f4a451932ed3c2016cf97a1
Hartmut Schorrig <hartmut@vishia.org>
2012-10-14 00:00:00 +0200

    * WriteCurve_ifc

--- org/vishia/curves/WriteCurveBinary.java
+++ org/vishia/curves/WriteCurveBinary.java
@@ -5,11 +5,44 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
+import org.vishia.util.Timeshort;
+
 /**This class writes values from curves in a binary file with head.
  * @author Hartmut Schorrig
  *
  */
 public class WriteCurveBinary implements WriteCurve_ifc{
+
+  
+  /**Version, history and copyright/copyleft.
+   * <ul>
+   * <li>2012-10-12 created. Firstly used for {@link org.vishia.guiInspc.InspcCurveView} and {@link org.vishia.gral.base.GralCurveView}.
+   * </ul>
+   * 
+   * <b>Copyright/Copyleft</b>:<br>
+   * For this source the LGPL Lesser General Public License,
+   * published by the Free Software Foundation is valid.
+   * It means:
+   * <ol>
+   * <li> You can use this source without any restriction for any desired purpose.
+   * <li> You can redistribute copies of this source to everybody.
+   * <li> Every user of this source, also the user of redistribute copies
+   *    with or without payment, must accept this license for further using.
+   * <li> But the LPGL is not appropriate for a whole software product,
+   *    if this source is only a part of them. It means, the user
+   *    must publish this part of source,
+   *    but doesn't need to publish the whole source of the own product.
+   * <li> You can study and modify (improve) this source
+   *    for own using or for redistribution, but you have to license the
+   *    modified sources likewise under this LGPL Lesser General Public License.
+   *    You mustn't delete this Copyright/Copyleft inscription in this source file.
+   * </ol>
+   * If you intent to use this source without publishing its usage, you can get
+   * a second license subscribing a special contract with the author. 
+   * 
+   * @author Hartmut Schorrig = hartmut.schorrig@vishia.de
+   */
+  public static final int version = 20121012;
 
   
   File fOut;
@@ -57,8 +90,7 @@ public class WriteCurveBinary implements WriteCurve_ifc{
   }
 
   @Override
-  public void writeCurveTimestamp(int timeshort, long timeAbs,
-      float millisec7short) {
+  public void writeCurveTimestamp(Timeshort xx) {
     // TODO Auto-generated method stub
     
   }
--- org/vishia/curves/WriteCurveCsv.java
+++ org/vishia/curves/WriteCurveCsv.java
@@ -7,25 +7,58 @@ import java.io.Writer;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.vishia.util.Timeshort;
+
 public class WriteCurveCsv  implements WriteCurve_ifc{
 
-  
+  /**Version, history and copyright/copyleft.
+   * <ul>
+   * <li>2012-10-12 created. Firstly used for {@link org.vishia.guiInspc.InspcCurveView} and {@link org.vishia.gral.base.GralCurveView}.
+   * </ul>
+   * 
+   * <b>Copyright/Copyleft</b>:<br>
+   * For this source the LGPL Lesser General Public License,
+   * published by the Free Software Foundation is valid.
+   * It means:
+   * <ol>
+   * <li> You can use this source without any restriction for any desired purpose.
+   * <li> You can redistribute copies of this source to everybody.
+   * <li> Every user of this source, also the user of redistribute copies
+   *    with or without payment, must accept this license for further using.
+   * <li> But the LPGL is not appropriate for a whole software product,
+   *    if this source is only a part of them. It means, the user
+   *    must publish this part of source,
+   *    but doesn't need to publish the whole source of the own product.
+   * <li> You can study and modify (improve) this source
+   *    for own using or for redistribution, but you have to license the
+   *    modified sources likewise under this LGPL Lesser General Public License.
+   *    You mustn't delete this Copyright/Copyleft inscription in this source file.
+   * </ol>
+   * If you intent to use this source without publishing its usage, you can get
+   * a second license subscribing a special contract with the author. 
+   * 
+   * @author Hartmut Schorrig = hartmut.schorrig@vishia.de
+   */
+  public static final int version = 20121012;
+
+
   File fOut;
   
   Writer out;
 
-  int absTimeshort;
+  Timeshort timeshortabs;
   
-  long absTime;
-  
-  float millisec7timeshort;
+  String[] sColumn;
   
   SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss.SSS");
   
   StringBuilder uLine = new StringBuilder(5000);
   
   @Override public void setTrackInfo(int nrofTracks, int ixTrack, String sPath, String sName){
-    
+    if(sColumn == null){
+      sColumn = new String[nrofTracks];
+    }
+    sColumn[ixTrack] = sPath;
   }
   
   public WriteCurveCsv() {
@@ -48,7 +81,7 @@ public class WriteCurveCsv  implements WriteCurve_ifc{
   }
 
   @Override public void writeCurveRecord(int timeshort, float[] values) throws IOException {
-    long time = (long)((timeshort - this.absTimeshort) * millisec7timeshort) + absTime;
+    long time = timeshortabs.absTimeshort(timeshort);
     Date date = new Date(time);
     uLine.setLength(0);
     String sDate = dateFormat.format(date);
@@ -65,13 +98,19 @@ public class WriteCurveCsv  implements WriteCurve_ifc{
       out.close();
     }
     out = new FileWriter(fOut);
-    
+    uLine.setLength(0);
+    uLine.append("time; ");
+    for(String sColumn1: sColumn){
+      if(sColumn1 == null){
+        sColumn1 = "?";
+      }
+      uLine.append(sColumn1).append(";  ");
+    }
+    out.append(uLine);
   }
 
-  @Override public void writeCurveTimestamp(int timeshort, long timeAbs, float millisec7short) {
-    this.absTimeshort = timeshort;
-    this.absTime = timeAbs;
-    this.millisec7timeshort = millisec7short;
+  @Override public void writeCurveTimestamp(Timeshort timeshortabs) {
+    this.timeshortabs = timeshortabs;
     
   }
   
