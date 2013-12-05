c7baf8e8a6fc0bfad28ddff67b0d3f31e31ead17
Hartmut Schorrig <hartmut@vishia.org>
2013-12-06 00:00:00 +0100

    * Address_InterProcessComm_Socket: {@link #toString()} does not returns the hostName, this needs communication and a DNS. Instead it returns the address (ip-number

--- org/vishia/communication/Address_InterProcessComm_Socket.java
+++ org/vishia/communication/Address_InterProcessComm_Socket.java
@@ -32,10 +32,45 @@ import java.net.*;
 public class Address_InterProcessComm_Socket 
 implements Address_InterProcessComm
 {
-    
-   public String toString() 
+
+  /**Version, history and license.
+   * <ul>
+   * <li>2013-12-05 Hartmut chg: {@link #toString()} does not returns the hostName, this needs communication 
+   *   and a DNS. Instead it returns the address (ip-number)
+   * <li>2005-01-01 Hartmut created.
+   * </ul>
+   * 
+   * <b>Copyright/Copyleft</b>:
+   * For this source the LGPL Lesser General Public License,
+   * published by the Free Software Foundation is valid.
+   * It means:
+   * <ol>
+   * <li> You can use this source without any restriction for any desired purpose.
+   * <li> You can redistribute copies of this source to everybody.
+   * <li> Every user of this source, also the user of redistribute copies
+   *    with or without payment, must accept this license for further using.
+   * <li> But the LPGL is not appropriate for a whole software product,
+   *    if this source is only a part of them. It means, the user
+   *    must publish this part of source,
+   *    but don't need to publish the whole source of the own product.
+   * <li> You can study and modify (improve) this source
+   *    for own using or for redistribution, but you have to license the
+   *    modified sources likewise under this LGPL Lesser General Public License.
+   *    You mustn't delete this Copyright/Copyleft inscription in this source file.
+   * </ol>
+   * If you are intent to use this sources without publishing its usage, you can get
+   * a second license subscribing a special contract with the author. 
+   * 
+   * @author Hartmut Schorrig = hartmut.schorrig@vishia.de
+   * 
+   */
+  //@SuppressWarnings("hiding")
+  public final static int version = 0x20131206;
+
+   @Override
+  public String toString() 
    {
-       String str = socketAddr.getHostName() + ":" + socketAddr.getPort();
+       String str = socketAddr.getAddress() + ":" + socketAddr.getPort();
        return str;
    }
   private InetSocketAddress socketAddr;
