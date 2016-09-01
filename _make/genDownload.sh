JzHartmut/srcJava_vishiaGui

genDownload.sh

_make/genDownload.sh
@@ -1,0 +1,3 @@
+export SRC="srcJava_vishiaGui"
+../../srcJava_vishiaBase/_make/+genDownload.sh
+

JzHartmut/srcJava_vishiaRun

_make/genDownload.sh
@@ -1,0 +1,3 @@
+export SRC="srcJava_vishiaRun"
+../../srcJava_vishiaBase/_make/+genDownload.sh
+