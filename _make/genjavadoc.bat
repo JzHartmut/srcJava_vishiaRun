JzHartmut/srcJava_vishiaBase

* genjavadoc supports zip of output tree (to provide for other locations)

_make/genjavadoc.bat
@@ -1,6 +1,7 @@
 
-set DST=..\..\docuSrcJava_Zbnf
-set DST_priv=..\..\docuSrcJava_Zbnf_priv
+set DSTDIR=..\..\
+set DST=docuSrcJava_Zbnf
+set DST_priv=docuSrcJava_Zbnf_priv
 
 echo set SRC
 set SRC=-subpackages org.vishia.zbnf

JzHartmut/srcJava_vishiaRun

_make/genjavadoc.bat
@@ -1,6 +1,7 @@
 
-set DST=..\..\docuSrcJava_vishiaRun
-set DST_priv=..\..\docuSrcJava_vishiaRun_priv
+set DSTDIR=..\..\
+set DST=docuSrcJava_vishiaRun
+set DST_priv=docuSrcJava_vishiaRun_priv
 
 echo set SRC
 set SRC=-subpackages org.vishia.inspector 